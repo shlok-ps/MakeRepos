4a9e3460945c670a2717fa1a4106fab31d81e1e6
shlok-ps <link2psp@gmail.com>
2024-09-28 10:14:41 +0000

    hello world

---
+++ index.js
@@ -0,0 +1,12 @@
+import OpenAI from "openai";
+const openAI = new OpenAI();
+
+const complation = await openAI.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+        {role: "system", content: "You are a helpful assistant"},
+        {role: "user", content: "Write a haiku about recursion in programming."}
+    ]
+})
+
+console.log(complation.choices[0].message);
